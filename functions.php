34112b430eb5991846932600cf4b56c3c8129161
Umer Irshad <umerirshad722@gmail.com>
2025-11-15 17:15:07 +0500

    step two complete

--- functions.php
+++ functions.php
@@ -9,10 +9,11 @@ function learn_theme_setup() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
     add_theme_support( 'custom-logo' );
-    add_theme_support( 'menu' );
+    add_theme_support( 'menus' );
 
-    register_nav_menu( array(
-        'primary_menu' => __( 'Primary Menu', 'learn-theme' ),
-        'footer_menu'  => __( 'Footer Menu', 'learn-theme' ),
+    register_nav_menus( array(
+        'primary_menu' => 'Primary Menu', 
+        'footer_menu'  => 'Footer Menu'
     ) );
 }
+add_action( 'after_setup_theme', 'learn_theme_setup' );
--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 <h1>Latest Posts</h1>
 <?php if (have_posts(  )): ?>
     <?php while (have_posts(  )) : the_post(); ?>
+    <article>
+        <h2><a href="<?php the_permalink( ); ?>"><?php the_title( ); ?></a></h2>
+        <?php if(has_post_thumbnail(  )): ?>
+            <div>
+                <?php the_post_thumbnail( 'medium' ); ?>
+            </div>
+            <?php endif; ?>
+            <?php the_excerpt(  ); ?>
+    </article>
 
+    <?php endwhile; ?>
+    <?php else: ?>
+        <p>No posts found.</p>
+<?php endif; ?>
 
 
 <?php get_footer( ); ?>
